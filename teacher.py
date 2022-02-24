f32d610fa19c43a326e658888db7d62c65339ba6
Vipin Kumar <vipinthakur3881@gmail.com>
2022-02-25 00:46:01 +0530

    adding FK relation between teacher and standard also add sub-OPTION for update standard of teacher and  get teacher by standard

--- teacher.py
+++ teacher.py
@@ -11,12 +11,14 @@ class Teacher:
     def __init__(self):
         pass
 
-    def teacher_map_return(self):
+    def teacher_option_map(self):
         return {
             1: self.get_list,
             2: self.add,
             3: self.update,
-            4: self.delete
+            4: self.delete,
+            5: self.associate_teacher_by_standard,
+            6: self.teacher_standard
         }
 
     def display_options(self):
@@ -25,22 +27,29 @@ class Teacher:
         print("2. Add teacher")
         print("3. Update teacher")
         print("4. Delete teacher")
-        print("5. Update standard of student")
-        print("6 Get student by standard")
+        print("5. Update standard of teacher")
+        print("6  Get teacher by standard")
 
         selected_input = int(input("Select option: "))
         if selected_input == 2:
             name = input("Enter student name:")
-            self.teacher_map_return().get(selected_input)(name)
+            self.teacher_option_map().get(selected_input)(name)
         elif selected_input == 3:
             name = input("Enter teacher name:")
             id = input("Enter teacher id:")
-            self.teacher_map_return().get(selected_input)(name, id)
+            self.teacher_option_map().get(selected_input)(name, id)
         elif selected_input == 4:
             id = input("Enter teacher id:")
-            self.teacher_map_return().get(selected_input)(id)
+            self.teacher_option_map().get(selected_input)(id)
+        elif selected_input == 5:
+            standard_id = int(input("Enter standard id:"))
+            teacher_id = int(input("Enter teacher id:"))
+            self.teacher_option_map().get(selected_input)(standard_id, teacher_id)
+        elif selected_input == 6:
+            standard_id = int(input("Enter standard id:"))
+            self.teacher_option_map().get(selected_input)(standard_id)
         else:
-            self.teacher_map_return().get(selected_input)()
+            self.teacher_option_map().get(selected_input)()
 
     """CRUD operations"""
 
@@ -76,3 +85,21 @@ class Teacher:
         cursor.execute(f"DELETE from {self.table} WHERE id={id};")
         connection.commit()
         print("Successfully deleted 1 record")
+
+    def associate_teacher_by_standard(self, standard_id, teacher_id):
+        connection = get_db_cursor()
+        cursor = connection.cursor()
+        cursor.execute(f"UPDATE {self.table} SET standard_id='{standard_id}' WHERE id='{teacher_id}'")
+        connection.commit()
+        connection.close()
+        print("Successfully updated record")
+
+    def teacher_standard(self, standard_id):
+        connection = get_db_cursor()
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM {self.table} WHERE standard_id= {standard_id}")
+        for teacher in cursor.fetchall():
+            print(teacher[1])
+        connection.commit()
+        connection.close()
+        print("Successfully update record")
